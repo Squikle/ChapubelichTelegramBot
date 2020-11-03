2887114bde381c042598734755f1e5424ba136b1
Squikle <55331475+Squikle@users.noreply.github.com>
2020-11-03 18:26:16 +0200

    Exception logger added

---
+++ Init/ExceptionLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace ChapubelichBot.Init
+{
+    [Serializable]
+    class ExceptionLogger
+    {
+        public string Time { get; set; }
+        public string Source { get; set; }
+        public string Message { get; set; }
+        public string CallStack { get; set; }
+        public ExceptionLogger InnerExeption { get; set; }
+
+        public ExceptionLogger(Exception ex)
+        {
+            Time = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+            Source = ex.Source;
+            Message = ex.Message;
+            CallStack = ex.StackTrace;
+            if (ex.InnerException != null)
+                InnerExeption = new ExceptionLogger(ex.InnerException);
+        }
+
+        public void WriteData(string path)
+        {
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+            string errorJson = JsonSerializer.Serialize(this, new JsonSerializerOptions() { WriteIndented = true });
+            Console.WriteLine(errorJson);
+            File.AppendAllText(path, errorJson);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,32 +12,21 @@ namespace ChapubelichBot
     {
         static void Main(string[] args)
         {
+            var config = Bot.GetConfig();
+            string path = Path.Combine(@"./Init/Config", config.GetValue<string>("Logger:DirectoryPath"), config.GetValue<string>("Logger:FileName"));
+            Console.WriteLine($"Default log path is: {path}"); 
+            AppDomain.CurrentDomain.UnhandledException += ExceptionManage;
             BotProcessor.StartReceiving();
             //var me = messageManager.client.GetMeAsync();
             //Console.Title = messageManager.client.GetMeAsync().Result.Username;
             Thread.Sleep(int.MaxValue);
-            AppDomain.CurrentDomain.UnhandledException += ExceptionManage;
         }
         private static void ExceptionManage(object sender, UnhandledExceptionEventArgs e)
         {
-            Exception ex = (Exception)e.ExceptionObject;
-            string error = $"Time: {DateTime.Now}\n" +
-                      $"Crashed: {ex.Source}\n" +
-                      $"Error message: {ex.Message}\n" +
-                      $"Last execute: {ex.StackTrace}\n";
-            if (ex.InnerException != null)
-                error += $"Inner exeption: {ex.InnerException}";
-            error += "\n\n";
-
             var config = Bot.GetConfig();
-
-            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), config.GetValue<string>("Logger:DirectoryPath"));
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
-            using (var writer = new StreamWriter(Path.Combine(path, config.GetValue<string>("Logger:FileName")), true, Encoding.Default))
-            {
-                writer.WriteLine(error);
-            }
+            string path = Path.Combine(@"./Init/Config", config.GetValue<string>("Logger:DirectoryPath"), config.GetValue<string>("Logger:FileName"));
+            ExceptionLogger exceptionLogger = new ExceptionLogger((Exception)e.ExceptionObject);
+            exceptionLogger.WriteData(path);
         }
     }
 }
