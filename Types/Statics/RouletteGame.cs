72b82d2b376bdc6376e3b5991f694d102aab5daf
Squikle <55331475+Squikle@users.noreply.github.com>
2020-11-15 20:13:36 +0200

    (refact) RouletteGame: migrated extensions to TelegramBotExtensions

--- Types/Statics/RouletteGame.cs
+++ Types/Statics/RouletteGame.cs
@@ -25,64 +25,6 @@ namespace ChapubelichBot.Types.Statics
         {
             return GameSessions.FirstOrDefault(x => x.ChatId == chatId);
         }
-        public static string ToEmoji(this RouletteColorEnum color)
-        {
-            switch (color)
-            {
-                case RouletteColorEnum.Red:
-                    return "\U0001F534";
-                case RouletteColorEnum.Black:
-                    return "\U000026AB";
-                case RouletteColorEnum.Green:
-                    return "\U0001F7E2";
-                default:
-                    return "";
-            }
-        }
-        public static RouletteColorEnum ToRouletteColor(this int number)
-        {
-            RouletteColorEnum[] rouletteTable = new RouletteColorEnum[TableSize]
-            {
-                RouletteColorEnum.Green, //0
-                RouletteColorEnum.Red,
-                RouletteColorEnum.Black,
-                RouletteColorEnum.Red,
-                RouletteColorEnum.Black,
-                RouletteColorEnum.Red, //5
-                RouletteColorEnum.Black,
-                RouletteColorEnum.Red,
-                RouletteColorEnum.Black,
-                RouletteColorEnum.Red,
-                RouletteColorEnum.Black, //10
-                RouletteColorEnum.Black,
-                RouletteColorEnum.Red,
-                RouletteColorEnum.Black,
-                RouletteColorEnum.Red,
-                RouletteColorEnum.Black, //15
-                RouletteColorEnum.Red,
-                RouletteColorEnum.Black,
-                RouletteColorEnum.Red,
-                RouletteColorEnum.Red,
-                RouletteColorEnum.Black, //20
-                RouletteColorEnum.Red,
-                RouletteColorEnum.Black,
-                RouletteColorEnum.Red,
-                RouletteColorEnum.Black,
-                RouletteColorEnum.Red, //25
-                RouletteColorEnum.Black,
-                RouletteColorEnum.Red,
-                RouletteColorEnum.Black,
-                RouletteColorEnum.Black,
-                RouletteColorEnum.Red, //30
-                RouletteColorEnum.Black,
-                RouletteColorEnum.Red,
-                RouletteColorEnum.Black,
-                RouletteColorEnum.Red,
-                RouletteColorEnum.Black, //35
-                RouletteColorEnum.Red
-            };
-            return rouletteTable[number];
-        }
         public static int[] GetBetsByNumbers(int firstNumber)
         {
             return new[]
@@ -169,31 +111,6 @@ namespace ChapubelichBot.Types.Statics
             }
 
             return null;
-        }
-        public static IEnumerable<RouletteBetToken> GroupByUsers(this IEnumerable<RouletteBetToken> listOfTokens)
-        {
-            List<RouletteBetToken> groupedList = new List<RouletteBetToken>();
-
-            foreach (var el in listOfTokens)
-            {
-                var token = groupedList.FirstOrDefault(x => x.UserId == el.UserId);
-                if (token != null)
-                    token.BetSum += el.BetSum;
-                else groupedList.Add(el);
-            }
-
-            return groupedList;
-        }
-        public static bool IsSequenceBy(this int[] array, int increaseNumber)
-        {
-            if (array == null || array.Length <= 2) 
-                return false;
-            for (int i=0; i<array.Length-1; i++)
-            {
-                if (array[i + 1] - array[i] != increaseNumber)
-                    return false;
-            }
-            return true;
         }
         public static bool IsAlreadyExist(long chatId)
         {
