795cf024cdf9a7ec81f2ebf5e64b0851460ea976
Squikle <55331475+Squikle@users.noreply.github.com>
2020-11-28 19:28:13 +0200

    (feature) added not implemented commands for crocodile game

---
+++ CommandEntities/CallbackCommands/CrocodileHostingCallback.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ChapubelichBot.Types.Abstractions.Commands;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ChapubelichBot.CommandEntities.CallbackCommands
+{
+    class CrocodileHostingCallback : CallbackCommand
+    {
+        public override List<string> IncludingData => new List<string> { "hostCrocodile" };
+        public override Task ExecuteAsync(CallbackQuery query, ITelegramBotClient client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CommandEntities/RegexCommands/Crocodile/CrocodileStartRegex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using ChapubelichBot.Types.Abstractions.Commands;
+using ChapubelichBot.Types.Managers.MessagesSender;
+using ChapubelichBot.Types.Statics;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ChapubelichBot.CommandEntities.RegexCommands.Crocodile
+{
+    class CrocodileStartRegex : RegexCommand
+    {
+        public override string Pattern => @"^\/? *(крокодил|crocodile)(@ChapubelichBot)?$";
+        public override async Task ExecuteAsync(Message message, ITelegramBotClient client)
+        {
+            throw new NotImplementedException();
+            await client.TrySendTextMessageAsync(message.Chat.Id,
+                "Игра \"Крокодил\"!" +
+                "\nНажмите на кнопку чтобы участвовать в выборе ведущего!",
+                replyToMessageId: message.MessageId,
+                replyMarkup: InlineKeyboards.CrocodileRegistration);
+        }
+    }
+}
