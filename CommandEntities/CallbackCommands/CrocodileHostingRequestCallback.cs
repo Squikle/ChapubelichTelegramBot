56368a65a48a1ac85b897c6168fad6f071618384
Squikle <55331475+Squikle@users.noreply.github.com>
2020-12-01 03:30:18 +0200

    (refact) renamed crocodile hosting request callback

---
+++ CommandEntities/CallbackCommands/CrocodileHostingRequestCallback.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ChapubelichBot.Types.Abstractions.Commands;
+using ChapubelichBot.Types.Managers;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ChapubelichBot.CommandEntities.CallbackCommands
+{
+    class CrocodileHostCandidacyCallback : CallbackCommand
+    {
+        public override List<string> IncludingData => new List<string> { "hostCrocodileRequest" };
+        public override async Task ExecuteAsync(CallbackQuery query, ITelegramBotClient client)
+        {
+            await CrocodileGameManager.AddToHostCandidatesRequestAsync(query);
+        }
+    }
+}
