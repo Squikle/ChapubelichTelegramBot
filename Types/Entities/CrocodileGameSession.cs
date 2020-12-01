145c0ec0109f10a7678bfc9b91daa250a686e652
Squikle <55331475+Squikle@users.noreply.github.com>
2020-12-01 02:08:04 +0200

    (refact) ICollection instead of list for host candidates

--- Types/Entities/CrocodileGameSession.cs
+++ Types/Entities/CrocodileGameSession.cs
@@ -10,7 +10,7 @@ namespace ChapubelichBot.Types.Entities
         public long ChatId { get; set; }
         public string Word { get; set; }
         public int GameMessageId { get; set; }
-        public List<User> HostCandidates { get; set; }
+        public ICollection<User> HostCandidates { get; set; }
         public User Host { get; set; }
         public DateTime LastActivity { get; set; }
     }
