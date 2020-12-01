169b52832a8a341a032fcdc300a5868aa214081f
Squikle <55331475+Squikle@users.noreply.github.com>
2020-12-01 20:27:26 +0200

    (feature) added callback command for choose 1 of 3 words

---
+++ CommandEntities/CallbackCommands/CrocodileChooseWordCallback.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChapubelichBot.Types.Abstractions.Commands;
+using ChapubelichBot.Types.Managers;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ChapubelichBot.CommandEntities.CallbackCommands
+{
+    class CrocodileChooseWordCallback : CallbackCommand
+    {
+        public override List<string> IncludingData => new List<string> { "crocodileChooseFirstWord", "crocodileChooseSecondWord", "crocodileChooseThirdWord" };
+        public override async Task ExecuteAsync(CallbackQuery query, ITelegramBotClient client)
+        {
+           await CrocodileGameManager.ChooseWordRequestTask(query);
+        }
+    }
+}
