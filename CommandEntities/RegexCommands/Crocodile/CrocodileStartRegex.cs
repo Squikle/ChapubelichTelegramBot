7069a1af57f4bd7176d3e536b1a90f48923d517b
Squikle <55331475+Squikle@users.noreply.github.com>
2020-12-01 03:32:03 +0200

    (feature) implemented start regex for crocodile game

--- CommandEntities/RegexCommands/Crocodile/CrocodileStartRegex.cs
+++ CommandEntities/RegexCommands/Crocodile/CrocodileStartRegex.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using ChapubelichBot.Types.Abstractions.Commands;
+using ChapubelichBot.Types.Managers;
 using ChapubelichBot.Types.Managers.MessagesSender;
 using ChapubelichBot.Types.Statics;
 using Telegram.Bot;
@@ -13,12 +14,7 @@ namespace ChapubelichBot.CommandEntities.RegexCommands.Crocodile
         public override string Pattern => @"^\/? *(крокодил|crocodile)(@ChapubelichBot)?$";
         public override async Task ExecuteAsync(Message message, ITelegramBotClient client)
         {
-            throw new NotImplementedException();
-            await client.TrySendTextMessageAsync(message.Chat.Id,
-                "Игра \"Крокодил\"!" +
-                "\nНажмите на кнопку чтобы участвовать в выборе ведущего!",
-                replyToMessageId: message.MessageId,
-                replyMarkup: InlineKeyboards.CrocodileRegistration);
+            await CrocodileGameManager.StartRequestAsync(message);
         }
     }
 }
