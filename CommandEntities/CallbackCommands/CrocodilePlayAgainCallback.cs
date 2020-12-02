4dc4661bb88f4299aefbd6b930181cfa09f52a0a
Squikle <55331475+Squikle@users.noreply.github.com>
2020-12-02 21:02:17 +0200

    (feature) crocodile play again callback message class

---
+++ CommandEntities/CallbackCommands/CrocodilePlayAgainCallback.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ChapubelichBot.Types.Abstractions.Commands;
+using ChapubelichBot.Types.Managers;
+using ChapubelichBot.Types.Managers.MessagesSender;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ChapubelichBot.CommandEntities.CallbackCommands
+{
+    class CrocodilePlayAgainCallback : CallbackCommand
+    {
+        public override List<string> IncludingData => new List<string> { "crocodilePlayAgain" };
+        public override async Task ExecuteAsync(CallbackQuery query, ITelegramBotClient client)
+        {
+            await client.TryEditMessageReplyMarkupAsync(query.Message.Chat.Id, query.Message.MessageId);
+            await CrocodileGameManager.CreateRequestAsync(query.Message);
+        }
+    }
+}
