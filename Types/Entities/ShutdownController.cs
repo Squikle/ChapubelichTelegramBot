2c8857603d0b2e44182f8e778cf8ea09f79210a8
Squikle <55331475+Squikle@users.noreply.github.com>
2020-12-06 19:06:46 +0200

    (feature) removed shutdown admin commands

--- Types/Entities/ShutdownController.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChapubelichBot.Types.Entities
-{
-    public static class ShutdownController
-    {
-        public static bool ShuttingDown = false;
-
-        public const int DefaultShutdown = 600;
-    }
-}
