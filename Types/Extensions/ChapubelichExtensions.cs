ae0c308bdf3b58b80d549d1fdb5ae35f4c0b5adb
Squikle <55331475+Squikle@users.noreply.github.com>
2020-12-09 02:15:06 +0200

    (refact) removed method for concurrency safe update in extensions

--- Types/Extensions/ChapubelichExtensions.cs
+++ Types/Extensions/ChapubelichExtensions.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using ChapubelichBot.Main.Chapubelich;
-using Microsoft.EntityFrameworkCore;
 
 namespace ChapubelichBot.Types.Extensions
 {
@@ -18,27 +14,6 @@ namespace ChapubelichBot.Types.Extensions
         public static IEnumerable<T> TakeTopValues<T>(this IEnumerable<T> source, int count) where T : IComparable<T>
         {
             return source.OrderByDescending(x => x).Distinct().Take(count);
-        }
-
-        public static async Task<int> ConcurrencyChangeValueAsync(this ChapubelichdbContext dbContext, Action changeValue)
-        {
-            int result = 0;
-            bool saveFailed;
-            do
-            {
-                saveFailed = false;
-                try
-                {
-                    changeValue();
-                    result = await dbContext.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException ex)
-                {
-                    saveFailed = true;
-                    await ex.Entries.Single().ReloadAsync();
-                }
-            } while (saveFailed);
-            return result;
         }
     }
 }
